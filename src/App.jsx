b29b70741502116d84d150a32b4a3f66d8fb3fa6
wsen0004 <wsen0004@student.monash.edu>
2023-08-18 16:54:33 +0800

    Added the card component which will display movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import { BiSearch } from "react-icons/bi";
+
 function App() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const API_URL = "http://www.omdbapi.com?apikey=10adcacc";
+
+  const searchMovie = async (title) => {
+    const responses = await fetch(API_URL + `&s=${title}`);
+    const data = await responses.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovie("");
+  }, []);
+
   return (
-    <>
-      <h1 class="bg-red-600">Hello world</h1>
-    </>
+    <div>
+      <h1 class="font-mono text-2xl tracking-wider text-center">My Contacts</h1>
+      <div class="flex gap-x-3 justify-center items-center">
+        <input
+          type="text"
+          placeholder="Enter a movie name"
+          name="movie-title"
+          value={movieTitle}
+          onChange={(e) => setMovieTitle(e.target.value)}
+        />
+        <BiSearch class="text-orange-500 text-2xl" />
+      </div>
+    </div>
   );
 }
 
