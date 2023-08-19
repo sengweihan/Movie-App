681c480b3ff135507e29065427d72f46f61ed80f
wsen0004 <wsen0004@student.monash.edu>
2023-08-19 14:52:51 +0800

    Completed the initial design for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import Card from "./Card";
 
 function App() {
   const [movieTitle, setMovieTitle] = useState("");
+  const [sendMovieTitle, setSendMovieTitle] = useState("");
+  const [movies, setMovies] = useState([]);
   const API_URL = "http://www.omdbapi.com?apikey=10adcacc";
 
-  const searchMovie = async (title) => {
-    const responses = await fetch(API_URL + `&s=${title}`);
+  const fetchMovies = async () => {
+    const responses = await fetch(
+      API_URL + `&s=${sendMovieTitle ? sendMovieTitle : "batman"}`
+    );
     const data = await responses.json();
+    setMovies(data.Search);
     console.log(data);
   };
 
   useEffect(() => {
-    searchMovie("");
-  }, []);
+    fetchMovies();
+  }, [sendMovieTitle]);
+
+  const handleSearch = () => {
+    setSendMovieTitle(movieTitle);
+  };
 
   return (
     <div>
@@ -26,7 +36,24 @@ function App() {
           value={movieTitle}
           onChange={(e) => setMovieTitle(e.target.value)}
         />
-        <BiSearch class="text-orange-500 text-2xl" />
+        <BiSearch onClick={handleSearch} />
+      </div>
+
+      <div class="grid grid-cols-3 gap-4 mt-5">
+        {movies.length > 0 ? (
+          movies.map((movie) => {
+            return (
+              <Card
+                key={movie.imdbID}
+                title={movie.Title}
+                year={movie.Year}
+                poster={movie.Poster}
+              />
+            );
+          })
+        ) : (
+          <h2>No movies found</h2>
+        )}
       </div>
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Card() {
-  return <div></div>;
+function Card({ title, year, poster }) {
+  return (
+    <div>
+      <img
+        src={poster !== "N/A" ? poster : "https://via.placeholder.com/400"}
+        alt={title}
+      />
+      <h2>{title}</h2>
+      <h2>{year}</h2>
+    </div>
+  );
 }
 
 export default Card;
